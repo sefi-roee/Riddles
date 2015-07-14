0fcd7274eba8b461f1a3bf9d4fb50029b0733ddb
Roee Sefi <roee.sefi@gmail.com>
2015-07-14 18:36:20 +0300

    Update "ShapeShifter"
    Up to level 31
    Add some optimizations

--- ShapeShifter/shapeshifter.py
+++ ShapeShifter/shapeshifter.py
@@ -49,7 +49,7 @@ class shapeshifter:
             s += 'Piece #{} ({}x{}):'.format(p, len(self.pieces[p]), len(self.pieces[p][0])) + '\n'
             for i in range(len(self.pieces[p])):
                  s += '####' * (len(self.pieces[p][0])) + '#' + '\n'
-                 s += '# ' + ' # '.join(map(str, self.pieces[p][i])) + ' #' + '\n'
+                 s += '# ' + ' # '.join([('1' if t == 1 else ' ') for t in self.pieces[p][i]]) + ' #' + '\n'
             s += '####' * (len(self.pieces[p][0])) + '#' + '\n'
             s += '\n'
 
@@ -63,16 +63,46 @@ class shapeshifter:
     def solve_helper(self, pieces, l, pos):
         if l == -1:
             if sum([self.board[i][j] for i in range(self.board_size[0]) for j in range(self.board_size[1])]) == 0:
-                print (sorted(pos))
+                print ('Solution:')
+                for p in sorted(pos):
+                    print ('Piece #{}, Pos: {},{}'.format(p[0], p[1][0], p[1][1]))
                 return True
 
             return False
 
-        
+        flips_req = sum([((self.X - self.board[i][j]) % self.X) for i in range(self.board_size[0]) for j in range(self.board_size[1])])
+        flips_have = 0
+        for i in range(l, -1, -1):
+            for r in range(len(pieces[i][2])):
+                for c in range(len(pieces[i][2][0])):
+                    flips_have += pieces[i][2][r][c]
+        flips_have = flips_have
+
+        eq = False
+        if flips_have < flips_req != 0:
+            return False
+        elif flips_have == flips_req:
+            eq = True
+            
         p_i, p = pieces[l][1], pieces[l][2]
-       
+
+        if l > 6:
+            print(l)
         for i in range(self.board_size[0] - len(p) + 1):
             for j in range(self.board_size[1] - len(p[0]) + 1):
+                if eq == True:
+                    br = False
+                    for a in range(len(p)):
+                        for b in range(len(p[0])):
+                            if self.board[i + a][j + b] == 0 and p[a][b] == 1:
+                                br = True
+                                break
+                        if br == True:
+                            break
+                                
+                    if br == True:
+                        continue
+                            
                 for a in range(len(p)):
                     for b in range(len(p[0])):
                         self.board[i + a][j + b] = (self.board[i + a][j + b] + p[a][b]) % self.X
@@ -86,7 +116,7 @@ class shapeshifter:
             
 if __name__ == "__main__":
     startTime = time.clock()
-    file_name = "shapeshifter_board21.txt"
+    file_name = "shapeshifter_board31.txt"
     game = shapeshifter(file_name)
     print (game)
     game.solve()
